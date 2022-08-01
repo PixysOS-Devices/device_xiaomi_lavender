758aace96c46597376a6c557cbd49f852c539f3d
OmarAlCoptan <omarcoptan9@gmail.com>
2022-08-01 09:48:33 +0200

    lavender: change TARGET_KERNEL_CONFIG
     * time for NekoLite kernel
    
    Signed-off-by: OmarAlCoptan <omarcoptan9@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,7 +60,7 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/lavender
-TARGET_KERNEL_CONFIG := lavender-perf_defconfig
+TARGET_KERNEL_CONFIG := lavender_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
 
 # QCOM hardware
